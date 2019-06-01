682cfe79a9f92b4a02a99743305a5695b915ecd7
Campbell Oleson <campbellsoleson@gmail.com>
2019-05-31 18:51:29 -0600

    W6D5

--- W6D5/towers_of_hanoi/lib/towers.rb
+++ W6D5/towers_of_hanoi/lib/towers.rb
@@ -1,11 +1,26 @@
-class TowersOfHanoi
-    attr_reader :towers
-    def initialize
-        @towers = [[1,2,3],[],[]]
+class Disk
+    attr_reader :size
+    def initialize(n)
+        @size = n
+    end
+end
+
+class Peg
+   attr_reader :disks
+    def initialize(dis = [])
+        @disks = []
+        dis.each do |d|
+            @disks << Disk.new(d)
+        end
     end
 
-    def valid_move?(disk,tower)
-        return false if tower.any? {|d|d < disk}
+    def inspect
         
+    end
 
 end
+
+peg1 = Peg.new([3,2,1])
+peg2 = Peg.new
+peg3 = Peg.new
+
--- W6D5/towers_of_hanoi/spec/towers_spec.rb
+++ W6D5/towers_of_hanoi/spec/towers_spec.rb
@@ -1,22 +1,41 @@
-require 'spec_helper'
 require 'towers'
 require 'rspec'
 
-describe TowersOfHanoi do
-    subject(:t) {TowersOfHanoi.new}
+describe Disk do 
+    subject(:disk) {Disk.new(5)}
     describe "#initialize" do 
-        it "Should initialize with a 2D array representing stacks" do 
-            expect(t.towers.count).to eq(3)
-            expect(t.towers[0]).to be_a Array
-        end 
-        it "The first stack should contain 3 integers representing disks" do
-            expect(t.towers[0][0]).to be_a Integer
-            expect(t.towers[0].count).to eq(3)
-        end
-        it "Stacks 2 and 3 should initialize as empty arrays" do
-            expect(t.towers[1]).to be_empty
-            expect(t.towers[2]).to be_empty
+        it "Should initialize Disk with an integer representing size" do
+            expect(disk.size).to eq(5)
         end
     end
 end
 
+describe Peg do 
+    subject(:peg) {Peg.new}
+    describe "#initialize" do 
+        context "When no array is passed in" do
+            it "Should initialize with an empty array by default" do 
+                expect(peg.disks).to be_empty
+            end
+        end
+
+    context "When an array is passed in" do
+        let(:peg) {Peg.new([4,3,2,1])}
+        it "Should initialize with the array" do 
+            expect(peg.disks.count).to eq(4)
+        end
+        
+        it "Should initialize the array with instances of disks" do 
+            expect(peg.disks.all?).to be_a(Disk)
+        end
+
+    end
+end
+end
+
+
+
+RSpec.configure do |config|
+    config.color = true
+end
+  
